6bb0f754ba5bc2e606b92c3db79ddcb9e7ce3d04
Lucas Walkner <Lucas.Walkner@copadata.com>
2022-08-11 09:16:50 +0200

    Initial commit

---
+++ Statusbits/Controller/StatusbitsController.cs
@@ -0,0 +1,232 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Statusbits.Controller;
+using Statusbits.Model;
+using System.Resources;
+using System.Diagnostics;
+
+namespace Statusbits.Controller
+{
+    class StatusbitsController
+    {
+        public StatusbitsModel Model { get; set; }
+        private BitDecryption.BitDecryption CalculateBits;
+
+        ResourceManager resource;
+
+        private Dictionary<string, BitDecryption.BitDecryption.BaseType> baseTypeHelper;
+        public StatusbitsController(int bits, string version = "1000")
+        {
+            CalculateBits = new BitDecryption.BitDecryption();
+            Model = new StatusbitsModel();
+
+            Model.Bit = bits;
+            Model.Version = version;
+
+            //set default value for decimal, signedDecimal, Hexadecimal, binary
+            Model.Values.Add("0");
+            Model.Values.Add("0");
+            Model.Values.Add("0");
+            Model.Values.Add("0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000");
+
+            //Load bits from Version
+            UpdateStatusbitsFromVersion(Model.Version);
+
+            //Load COT Messages
+            resource = new ResourceManager("Statusbits.COT", typeof(StatusbitsController).Assembly);
+            for (var i = 0; i < Model.Bit; i++)
+            {
+                Model.CotMessages.Add(resource.GetString(i.ToString()));
+            }
+            Model.CotValue = 0;
+            Model.CotMessage = Model.CotMessages[Model.CotValue];
+
+            SetLanguage("en");
+        }
+
+        //Calculate values from input and set checkboxes
+        public void UpdateValues(string value, string baseForm, IList items8, IList items7, IList items6, IList items5, IList items4, IList items3, IList items2, IList items1)
+        {
+            Model.ErrorMsg = "";
+            try
+            {
+                var Bit = BitDecryption.BitDecryption.Bitness.Bit64;
+                if (Model.Bit == (int)BitDecryption.BitDecryption.Bitness.Bit32)
+                {
+                    Bit = BitDecryption.BitDecryption.Bitness.Bit32;
+                }
+                Model.Values = CalculateBits.CalculateFromBase(value, Bit, baseTypeHelper[baseForm]);
+                CalculateBits.CalculateActiveCheckboxes(Model.StatusBits, items8, items7, items6, items5, items4, items3, items2, items1, value, Bit, baseTypeHelper[baseForm]);
+                
+                if (Model.Bit == 64)
+                {
+                    Model.CotValue = CalculateBits.CalculateCOT(Model.StatusBits, items5);
+                    Model.CotMessage = Model.CotMessages[Model.CotValue];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Model.ErrorMsg = e.ToString();
+            }
+        }
+
+        //Update Values from given Checkboxes
+        public void UpdateFromCheckboxes(IList items8, IList items7, IList items6, IList items5, IList items4, IList items3, IList items2, IList items1)
+        {
+            
+            try
+            {
+                var Bit = BitDecryption.BitDecryption.Bitness.Bit64;
+                if (Model.Bit == (int)BitDecryption.BitDecryption.Bitness.Bit32)
+                {
+                    Bit = BitDecryption.BitDecryption.Bitness.Bit32;
+                }
+
+
+                Model.Values = CalculateBits.CalculateFromCheckBoxes(items1, items2, items3, items4, items5, items6, items7, items8, Model.StatusBits, Bit);
+                if (Model.Bit == 64)
+                {
+                    Model.CotValue = CalculateBits.CalculateCOT(Model.StatusBits, items5);
+                    Model.CotMessage = Model.CotMessages[Model.CotValue];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void UpdateStatusbitsFromVersion(string version)
+        {
+            try
+            {
+                Model.Version = version;
+                resource = new ResourceManager("Statusbits.Version." + version, typeof(StatusbitsController).Assembly);
+
+                List<string> newStatusbits = new List<string>();
+                List<string> newStatusbits1 = new List<string>();
+                List<string> newStatusbits2 = new List<string>();
+                List<string> newStatusbits3 = new List<string>();
+                List<string> newStatusbits4 = new List<string>();
+                List<string> newStatusbits5 = new List<string>();
+                List<string> newStatusbits6 = new List<string>();
+                List<string> newStatusbits7 = new List<string>();
+                List<string> newStatusbits8 = new List<string>();
+
+                for (var i = 0; i < Model.Bit; i++)
+                {
+                    newStatusbits.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                    switch (i / 8)
+                    {
+                        case 0:
+                            newStatusbits1.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 1:
+                            newStatusbits2.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 2:
+                            newStatusbits3.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 3:
+                            newStatusbits4.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 4:
+                            newStatusbits5.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 5:
+                            newStatusbits6.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 6:
+                            newStatusbits7.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+
+                        case 7:
+                            newStatusbits8.Add(i.ToString() + " " + resource.GetString(i.ToString()));
+                            break;
+                    }
+                }
+                newStatusbits.Reverse();
+                newStatusbits1.Reverse();
+                newStatusbits2.Reverse();
+                newStatusbits3.Reverse();
+                newStatusbits4.Reverse();
+                newStatusbits5.Reverse();
+                newStatusbits6.Reverse();
+                newStatusbits7.Reverse();
+                newStatusbits8.Reverse();
+
+                Model.StatusBits = newStatusbits;
+                Model.StatusBits1 = newStatusbits1;
+                Model.StatusBits2 = newStatusbits2;
+                Model.StatusBits3 = newStatusbits3;
+                Model.StatusBits4 = newStatusbits4;
+                Model.StatusBits5 = newStatusbits5;
+                Model.StatusBits6 = newStatusbits6;
+                Model.StatusBits7 = newStatusbits7;
+                Model.StatusBits8 = newStatusbits8;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Model.ErrorMsg = e.ToString();
+            }
+        }
+
+        public void UpdateBits(int bits)
+        {
+            try
+            {
+                Model.Bit = bits;
+                UpdateStatusbitsFromVersion(Model.Version);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void SetLanguage(string language)
+        {
+            Model.Language = language;
+            resource = new ResourceManager("Statusbits.Strings.Resources_" + language, typeof(StatusbitsController).Assembly);
+            Model.COT_Resources = resource.GetString("CauseOfTransmission");
+            Model.Language_Resources = resource.GetString("Language");
+            Model.ScanClipboard_Resources = resource.GetString("ScanFromClipboard");
+            Model.Version_Resources = resource.GetString("Version");
+            Model.No_Resources = resource.GetString("No");
+            Model.Hex_Resources = resource.GetString("Hex");
+            Model.Dec_Resources = resource.GetString("Dec");
+            Model.Bin_Resources = resource.GetString("Bin");
+
+            //Add Clipboard options
+            Model.ClipboardOptions.Clear();
+            Model.ClipboardOptions.Add(Model.No_Resources);
+            Model.ClipboardOptions.Add(Model.Dec_Resources);
+            Model.ClipboardOptions.Add(Model.Hex_Resources);
+            Model.ClipboardOptions.Add(Model.Bin_Resources);
+            //Model.ClipboardOptions.Add("signed decimal");
+
+            //create baseTypeHelper
+            baseTypeHelper = new Dictionary<string, BitDecryption.BitDecryption.BaseType>();
+
+            baseTypeHelper.Add(Model.Dec_Resources, BitDecryption.BitDecryption.BaseType.Decimal);
+            //baseTypeHelper.Add("SignedDecimal", BitDecryption.BitDecryption.BaseType.SignedDecimal);
+            baseTypeHelper.Add(Model.Hex_Resources, BitDecryption.BitDecryption.BaseType.Hex);
+            baseTypeHelper.Add(Model.Bin_Resources, BitDecryption.BitDecryption.BaseType.Binary);
+        }
+
+    }
+}
---
+++ Statusbits/Model/StatusbitsModel.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Statusbits.Model
+{
+    public class StatusbitsModel : INotifyPropertyChanged
+    {
+
+        private string _version = "";
+        public string Version
+        {
+            get => _version;
+            set { _version = value; OnPropertyChanged(nameof(Version)); }
+        }
+
+        private string _errorMsg = "";
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            set { _errorMsg = value; OnPropertyChanged(nameof(ErrorMsg)); }
+        }
+
+        private int _bit;
+        public int Bit
+        {
+            get => _bit;
+            set { _bit = value; OnPropertyChanged(nameof(Bit)); }
+        }
+
+        private string _calculationType = "";
+        public string CalculationType
+        {
+            get => _calculationType;
+            set { _calculationType = value; OnPropertyChanged(nameof(CalculationType)); }
+        }
+
+        private string _language = "";
+        public string Language
+        {
+            get => _language;
+            set { _language = value; OnPropertyChanged(nameof(Language)); }
+        }
+
+        private string _Language_Resources = "";
+        public string Language_Resources
+        {
+            get => _Language_Resources;
+            set { _Language_Resources = value; OnPropertyChanged(nameof(Language_Resources)); }
+        }
+
+        private string _COT_Resources = "";
+        public string COT_Resources
+        {
+            get => _COT_Resources;
+            set { _COT_Resources = value; OnPropertyChanged(nameof(COT_Resources)); }
+        }
+
+        private string _no_Resources = "";
+        public string No_Resources
+        {
+            get => _no_Resources;
+            set { _no_Resources = value; OnPropertyChanged(nameof(No_Resources)); }
+        }
+        private string _dec_Resources = "";
+        public string Dec_Resources
+        {
+            get => _dec_Resources;
+            set { _dec_Resources = value; OnPropertyChanged(nameof(Dec_Resources)); }
+        }
+        private string _hex_Resources = "";
+        public string Hex_Resources
+        {
+            get => _hex_Resources;
+            set { _hex_Resources = value; OnPropertyChanged(nameof(Hex_Resources)); }
+        }
+        private string _bin_Resources = "";
+        public string Bin_Resources
+        {
+            get => _bin_Resources;
+            set { _bin_Resources = value; OnPropertyChanged(nameof(Bin_Resources)); }
+        }
+
+        private string _ScanClipboard_Resources = "";
+        public string ScanClipboard_Resources
+        {
+            get => _ScanClipboard_Resources;
+            set { _ScanClipboard_Resources = value; OnPropertyChanged(nameof(ScanClipboard_Resources)); }
+        }
+
+        private string _Version_Resources = "";
+        public string Version_Resources
+        {
+            get => _Version_Resources;
+            set { _Version_Resources = value; OnPropertyChanged(nameof(Version_Resources)); }
+        }
+
+        private int _cotValue;
+        public int CotValue
+        {
+            get => _cotValue;
+            set { _cotValue = value; OnPropertyChanged(nameof(CotValue)); }
+        }
+
+        private string _cotMessage = "";
+        public string CotMessage
+        {
+            get => _cotMessage;
+            set { _cotMessage = value; OnPropertyChanged(nameof(CotMessage)); }
+        }
+
+        private List<string> _cotMessages = new List<string>();
+        public List<string> CotMessages
+        {
+            get => _cotMessages;
+            set { _cotMessages = value; OnPropertyChanged(nameof(CotMessages)); }
+        }
+
+        private List<string> _statusBits = new List<string>();
+        public List<string> StatusBits
+        {
+            get => _statusBits;
+            set { _statusBits = value; OnPropertyChanged(nameof(StatusBits)); }
+        }
+
+        private List<string> _statusBits8 = new List<string>();
+        public List<string> StatusBits8
+        {
+            get => _statusBits8;
+            set { _statusBits8 = value; OnPropertyChanged(nameof(StatusBits8)); }
+        }
+
+        private List<string> _statusBits7 = new List<string>();
+        public List<string> StatusBits7
+        {
+            get => _statusBits7;
+            set { _statusBits7 = value; OnPropertyChanged(nameof(StatusBits7)); }
+        }
+
+        private List<string> _statusBits6 = new List<string>();
+        public List<string> StatusBits6
+        {
+            get => _statusBits6;
+            set { _statusBits6 = value; OnPropertyChanged(nameof(StatusBits6)); }
+        }
+
+        private List<string> _statusBits5 = new List<string>();
+        public List<string> StatusBits5
+        {
+            get => _statusBits5;
+            set { _statusBits5 = value; OnPropertyChanged(nameof(StatusBits5)); }
+        }
+
+        private List<string> _statusBits4 = new List<string>();
+        public List<string> StatusBits4
+        {
+            get => _statusBits4;
+            set { _statusBits4 = value; OnPropertyChanged(nameof(StatusBits4)); }
+        }
+
+        private List<string> _statusBits3 = new List<string>();
+        public List<string> StatusBits3
+        {
+            get => _statusBits3;
+            set { _statusBits3 = value; OnPropertyChanged(nameof(StatusBits3)); }
+        }
+
+        private List<string> _statusBits2 = new List<string>();
+        public List<string> StatusBits2
+        {
+            get => _statusBits2;
+            set { _statusBits2 = value; OnPropertyChanged(nameof(StatusBits2)); }
+        }
+
+        private List<string> _statusBits1 = new List<string>();
+        public List<string> StatusBits1
+        {
+            get => _statusBits1;
+            set { _statusBits1 = value; OnPropertyChanged(nameof(StatusBits1)); }
+        }
+
+        private List<string> _clipboardOptions = new List<string>();
+        public List<string> ClipboardOptions
+        {
+            get => _clipboardOptions;
+            set { _clipboardOptions = value; OnPropertyChanged(nameof(ClipboardOptions)); }
+        }
+
+        private List<string> _values = new List<string>();
+        public List<string> Values
+        {
+            get => _values;
+            set { _values = value; OnPropertyChanged(nameof(Values)); }
+        }
+
+        private IList _items = new List<object>();
+        public IList Items
+        {
+            get => _items;
+            set { _items = value; OnPropertyChanged(nameof(Items)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
